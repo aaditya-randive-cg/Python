8cb5ebc765fe7e2cbe8112abff2cd3142c18b8df
aaditya-randive-cg <aaditya.randive.cg@gmail.com>
2026-09-07 09:06:47 +0530

    add file

---
+++ 08.conditional_statements/08.conditional_statements.py
@@ -0,0 +1,63 @@
+#a basics of 
+#question1
+number=int(input('Enter the number'))
+if number >10:
+    print("Greater than 10")
+
+#question2
+person=int(input("Enter Age"))
+if person >= 18:
+    print("Adult")
+
+#question3
+number=int(input('Enter the number'))
+if number >0:
+    print("positive")
+
+#question4
+marks=int(input("Enter marks"))
+if marks>=40:
+    print("pass")
+
+#question5
+number=int(input('Enter the number'))
+if number==0:
+    print("Zero")
+
+#question6
+number=int(input('Enter the number'))
+if number<0:
+    print("Not positive")
+if number<0 or number==0:
+    print("positive")
+
+#question7
+number=int(input("Enter number"))
+if  number<18:
+    print("Adult")
+else:
+    print("Minor")
+
+#question8
+number=int(input("Enter number"))
+if number %2==0:
+    print("even")
+else:
+    print("odd")
+
+#question9
+marks=int(input("Enter number"))
+if marks>=40:
+    print("pass")
+else:
+    print("fail")
+
+#question10
+number1=int(input("Enter "))
+number2=int(input("Enter"))
+if number2>number1:
+    print("number2 is greater")
+if number1>number2:
+    print("number1 is greater")
+else:
+    print("equal")
