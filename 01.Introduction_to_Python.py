3d25aa0acf457e306fb61e53c4c23bcfa53ce8e7
aaditya-randive-cg <aaditya.randive.cg@gmail.com>
2026-09-07 08:49:24 +0530

    add these file

---
+++ 01.Introduction_to_Python.py
@@ -0,0 +1,9 @@
+#15
+# This program displays a welcome message to the user.
+
+print("Welcome to Python Programming!")
+
+#16
+# Python is a high-level, easy-to-learn programming language.
+
+print("I am learning Python and enjoying programming!")
---
+++ 03.python_data_type_and_type_casting.py
@@ -0,0 +1,14 @@
+a=27
+print(a)
+print(type(a))
+a=bool(a)
+print(a)
+print(type(a))
+a=float(a)
+print(a)
+print(type(a))
+print(a)
+a=str(a)
+print(a)
+print(type(a))
+
