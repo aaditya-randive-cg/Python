6f86d80426610c4130951cd47ae9246313d31b80
aaditya-randive-cg <aaditya.randive.cg@gmail.com>
2026-09-07 09:11:36 +0530

    deleted files

--- 01.Introduction_to_Python.py
+++
@@ -1,9 +0,0 @@
-#15
-# This program displays a welcome message to the user.
-
-print("Welcome to Python Programming!")
-
-#16
-# Python is a high-level, easy-to-learn programming language.
-
-print("I am learning Python and enjoying programming!")
